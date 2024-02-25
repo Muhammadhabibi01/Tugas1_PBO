1868c2743ff18a201699e987b387c84497cd71cf
Muh Habibi <88193875+Muhammadhabibi01@users.noreply.github.com>
2024-02-25 18:44:01 +0800

    Add files via upload

---
+++ Tugas1_SourceCode_Praktek/Habibi_13020220117/src/Asign.java
@@ -0,0 +1,12 @@
+/*
+Nim  : 13020220117
+Nama : Muh.Habibi
+--Sabtu, 21/02/2024--
+ */
+public class Asign {
+    public static void main (String[] args){
+        int i;
+        System.out.print("Hello\n"); i = 5;
+        System.out.println("Ini nilai i : " + i);
+    }
+}
