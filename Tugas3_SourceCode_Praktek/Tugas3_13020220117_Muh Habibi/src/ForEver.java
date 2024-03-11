d506c0ad4d4fc39f2feb7bfc0c25a3cf6686accc
Muh Habibi <88193875+Muhammadhabibi01@users.noreply.github.com>
2024-03-11 21:36:19 +0800

    Add files via upload

---
+++ Tugas3_SourceCode_Praktek/Tugas3_13020220117_Muh Habibi/src/ForEver.java
@@ -0,0 +1,13 @@
+/*
+Nim  : 13020220117
+Nama : Muh. Habibi
+Jumat, 08/03/2024
+ */
+public class ForEver {
+    public static void  main(String[] args){
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while(true){
+            System.out.print("Print satu baris ...\n");
+        }
+    }
+}
