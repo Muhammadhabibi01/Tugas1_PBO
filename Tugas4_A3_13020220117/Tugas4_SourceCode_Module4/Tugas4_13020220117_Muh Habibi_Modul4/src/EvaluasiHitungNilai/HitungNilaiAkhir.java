7f9eb9cc9bad5eec19bc65845622b75f025f1965
Muh Habibi <88193875+Muhammadhabibi01@users.noreply.github.com>
2024-03-28 00:30:49 +0800

    Add files via upload

---
+++ Tugas4_A3_13020220117/Tugas4_SourceCode_Module4/Tugas4_13020220117_Muh Habibi_Modul4/src/EvaluasiHitungNilai/HitungNilaiAkhir.java
@@ -0,0 +1,18 @@
+/*
+Nama    : Muh. Habibi
+Nim     : 13020220117
+Kelas   : A3
+Kamis, 21/03/2024
+ */
+package EvaluasiHitungNilai;
+
+public class HitungNilaiAkhir {
+    public double nilaiTugas(int tugas1, int tugas2) {
+        return (tugas1 + tugas2) / 2.0;
+    }
+    public double nilaiAkhir(double tugas, int mid, int uas) {
+        return (tugas * 0.4) + (mid * 0.3) + (uas * 0.3);
+    }
+}
+
+
---
+++ Tugas4_A3_13020220117/Tugas4_SourceCode_Module4/Tugas4_13020220117_Muh Habibi_Modul4/src/EvaluasiMahasiswa/Identitas.java
@@ -0,0 +1,28 @@
+/*
+Nama    : Muh. Habibi
+Nim     : 13020220117
+Kelas   : A3
+Kamis, 21/03/2024
+ */
+package EvaluasiMahasiswa;
+public class Identitas {
+    private String nama;
+    private String stambuk;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setStambuk(String stambuk) {
+        this.stambuk = stambuk;
+    }
+
+    public String getStambuk() {
+        return this.stambuk;
+    }
+}
+
